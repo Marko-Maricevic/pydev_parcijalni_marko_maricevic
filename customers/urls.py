b1de6bc789ba5cabb1b8ee922da80390af1bd51c
Marko Maričević <mrksonop@gmail.com>
2025-05-17 13:07:37 +0200

    sum

--- customers/urls.py
+++ customers/urls.py
@@ -5,6 +5,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', CustomerListView.as_view(), name='customer_list'),
-    path('new/', CustomerCreateView.as_view(), name='customer_create'),
-    path('<int:pk>/edit/', CustomerUpdateView.as_view(), name='customer_update'),
+    path('new/', CustomerCreateView.as_view(), name='customer_create'),  
 ]
